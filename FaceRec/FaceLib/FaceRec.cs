55c02b18b6d73b9a0443347a98cee5c83b179643
Felipe Quintella <felipe@quintella.com>
2024-12-22 12:38:09 -0300

    project commit

--- FaceRec/FaceLib/FaceRec.cs
+++ FaceRec/FaceLib/FaceRec.cs
@@ -1,6 +1,39 @@
+using System.ComponentModel;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+
 namespace FaceLib;
 
 public class FaceRec
 {
+    private double distance = 1E+19;
+    private CascadeClassifier CascadeClassifier = new CascadeClassifier(Environment.CurrentDirectory + "/Haarcascade/haarcascade_frontalface_alt.xml");
+    private Image<Bgr, byte> Frame = (Image<Bgr, byte>) null;
+//private Emgu.CV.Capture camera;
+    private Mat mat = new Mat();
+    private List<Image<Gray, byte>> trainedFaces = new List<Image<Gray, byte>>();
+    private List<int> PersonLabs = new List<int>();
+    private bool isEnable_SaveImage = false;
+    private string ImageName;
+    private string setPersonName;
+    public bool isTrained = false;
+    private List<string> Names = new List<string>();
+    private EigenFaceRecognizer eigenFaceRecognizer;
+    private IContainer components = (IContainer) null;
+    
+    public FaceRec()
+    {
+        this.InitializeComponent();
+        if (Directory.Exists(Environment.CurrentDirectory + "\\Image"))
+            return;
+        Directory.CreateDirectory(Environment.CurrentDirectory + "\\Image");
+    }
+    
+    private void InitializeComponent()
+    {
+        this.eigenFaceRecognizer = new EigenFaceRecognizer(80, double.PositiveInfinity);
+    }
     
 }
---
+++ FaceRec/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace FaceRec.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
